97311b962ecf3bf13c85607f42c93ae52756aea3
aditi-python <aditi.ghalsasi@gmail.com>
2014-06-17 20:04:35 -0700

    pythonroom update

--- athomepython.py
+++ athomepython.py
@@ -1,14 +1,2 @@
 # author: aditi-python
 
-import turtle
-t = turtle.Turtle()
-angle = 71
-
-numbers = (10, 200)
-colors = ("red" , "blue" , "pink" , "purple" , "yellow", "green" , "orange")
-
-for number in numbers:
-	for color in colors:
-		t.color(color)
-		t.forward(100)
-		t.left(angle)
